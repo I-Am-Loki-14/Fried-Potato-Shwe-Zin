0372a4a5e28823d080f6ced73e9eaef51c45ac2c
Min Naing Oo <minnaingoo792@gmail.com>
2026-07-03 12:47:27 +0630

    modified-home-section

--- js/javascript.js
+++ js/javascript.js
@@ -25,3 +25,63 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+function blockMinus(event) {
+    if (event.key === '-' || event.key === 'Minus' || event.key === 'e') {
+        event.preventDefault();
+    }
+}
+
+
+function addToCart() {
+    const quantityInput = document.getElementById('quantity');
+    let currentQty = Number(quantityInput.value);
+    quantityInput.value = currentQty + 1; 
+    
+    calculatePrice();
+}
+
+
+function calculatePrice() {
+    const quantityInput = document.getElementById('quantity');
+    const totalDisplay = document.getElementById('totalDisplay');
+    const noteDisplay = document.getElementById('noteDisplay');
+
+    
+    if (quantityInput.value === "" || Number(quantityInput.value) === 0) {
+        totalDisplay.innerText = "0 ကျပ်";
+        noteDisplay.innerText = "အရေအတွက် ရိုက်ထည့်ပါ သို့မဟုတ် အပေါ်ကခလုတ်နှိပ်ပါ။";
+        noteDisplay.style.color = "#a0a0a0";
+        return;
+    }
+
+    let quantity = Number(quantityInput.value);
+    let pricePerPack = 2200;
+    let baseTotal = quantity * pricePerPack; 
+    let discount = 0;
+
+   
+    discount = Math.floor(quantity / 5) * 500;
+
+    
+    let finalTotal = baseTotal - discount;
+
+   
+    totalDisplay.innerText = finalTotal.toLocaleString() + " ကျပ်";
+
+    
+    if (discount > 0) {
+        
+        let nextMilestone = (Math.floor(quantity / 5) + 1) * 5;
+        let missingPacks = nextMilestone - quantity;
+
+        noteDisplay.innerText = `အထူးပရိုမိုးရှင်းဖြင့် ${discount.toLocaleString()} ကျပ် သက်သာသွားပါပြီ 🎉 \n (နောက်ထပ် ${missingPacks} ထုပ် ထပ်ယူရင် နောက်ထပ် ၅၀၀ ထပ်လျှော့ဦးမှာနော်! 🔥)`;
+        noteDisplay.style.color = "#2ed573";
+    } 
+    else {
+        
+        let missingPacks = 5 - quantity;
+        noteDisplay.innerText = `နောက်ထပ် ${missingPacks} ထုပ်ပဲ ထပ်ယူလိုက်ရင် ၅၀၀ ကျပ် တန်းလျှော့ပေးမှာနော်! 🔥`;
+        noteDisplay.style.color = "#ff4757"; 
+    }
+}
